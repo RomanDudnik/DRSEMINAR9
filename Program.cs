f3b32afd1eb988b800055af7698d069fdd182c86
«Roman <research777@mail.ru>
2022-12-12 21:57:08 +0300

    DRSEMINAR9_Ex2-3

--- Program.cs
+++ Program.cs
@@ -4,15 +4,18 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
+/*
 void ShowNumbers(int n)
 {
     Console.Write(n + " ");
-    if (n > 1) ShowNumbers (n - 1);
+    if (n > 1) 
+        ShowNumbers (n - 1);
 }
 
 Console.WriteLine("Input your number: ");
 int num = Convert.ToInt16(Console.ReadLine());
 ShowNumbers(num);
+*/
 
 // Задача 66: Задайте значения M и N. Напишите программу, 
 // которая найдёт сумму натуральных элементов в промежутке от M до N.
@@ -20,9 +23,57 @@ ShowNumbers(num);
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+/*
+int SumDigitsMN(int m, int n)
+{   
+    int sum = m;
+    if (m == n) 
+        return n;
+    else
+    {
+        m++;
+        sum = m - 1+ SumDigitsMN(m, n);
+        return sum;
+    }
+}
+
+Console.WriteLine("Input your first number: ");
+int firstNum = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Input your second number: ");
+int secondNum = Convert.ToInt16(Console.ReadLine());
+
+int sum = SumDigitsMN(firstNum, secondNum);
+Console.WriteLine($"The sum of the numbers of the segment from {firstNum} to {secondNum} is {sum}");
+*/
+
 // Задача 68 (дополнительно): дополнительно Напишите программу вычисления функции Аккермана с помощью рекурсии.
 // Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
+int AkkermanMethod(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return AkkermanMethod(m - 1, 1);
+    }
+    else
+    {
+        return (AkkermanMethod(m - 1, AkkermanMethod(m, n - 1)));
+    }
+}
+
+Console.WriteLine("Input your first number: ");
+int firstNum = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Input your second number: ");
+int secondNum = Convert.ToInt16(Console.ReadLine());
+
+int functionAkerman = AkkermanMethod(firstNum, secondNum);
+Console.WriteLine(functionAkerman);
+
 // Задача: Написать рекурсивную функцию перевода из десятичной системы исчисления в двоичную.
+
